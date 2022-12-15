c34a13343462568b5446ebef0a5ae572948ff828
ahsan-javaid <ahsan786cs@gmail.com>
2022-12-15 18:17:27 +0500

    feat:parse url from id

--- src/api/user.rs
+++ src/api/user.rs
@@ -36,3 +36,27 @@ pub fn create_user(r: Request) {
   let serialized = serde_json::to_string(&user).unwrap();
   r.handle_json(serialized);
 }
+
+pub fn get_user_by_id(r: Request) {
+  let connection = sqlite::open("db.db").unwrap();
+  let query = format!("SELECT * FROM users where id={}", r.param);
+  let mut users :Vec<User> = Vec::new();
+ 
+  connection.iterate(query, |pairs| {
+         let (_, id) = pairs.get(0).unwrap();
+         let (_, name) = pairs.get(1).unwrap();
+         let (_, email) = pairs.get(2).unwrap();
+ 
+         users.push(User {
+           id: id.unwrap().parse::<u32>().unwrap(),
+           name: String::from(name.unwrap()),
+           email: String::from(email.unwrap())
+         });
+ 
+         true
+     })
+     .unwrap();
+ 
+     let serialized = serde_json::to_string(&users[0]).unwrap();
+     r.handle_json(serialized);
+}
