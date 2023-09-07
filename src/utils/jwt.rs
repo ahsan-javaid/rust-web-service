9af02fdfe5b547a7cc3ef33461fbff033a7f1f78
ahsan-javaid <ahsan786cs@gmail.com>
2023-09-07 06:37:10 +0500

    chore: add jwt

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,4 +1,18 @@
-#[warn(dead_code)]
-pub fn create_jwt() -> String {
-  "jwt_token".to_string()
+use serde::{Deserialize, Serialize};
+use jsonwebtoken::{encode, Header, EncodingKey};
+use crate::models::user::*;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+   id: u32,
+   email: String
+}
+
+pub fn create_jwt(user: User) -> Result<String, jsonwebtoken::errors::Error> {
+  let my_claims = Claims {
+    id: user.id,
+    email: user.email.clone()
+  };
+
+   encode(&Header::default(), &my_claims, &EncodingKey::from_secret("secret".as_ref()))
 }
