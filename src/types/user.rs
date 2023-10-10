b62ae4083397dada75b025f9e36a9fa84c13a713
ahsan-javaid <ahsan786cs@gmail.com>
2023-10-10 06:18:29 +0500

    feat: create auth token

--- src/types/user.rs
+++ src/types/user.rs
@@ -1,3 +1,4 @@
+use crate::models::user::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -5,4 +6,10 @@ pub struct UserPayload {
     pub name: String,
     pub email: String,
     pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserResponse {
+    pub user: User,
+    pub token: String,
 }
