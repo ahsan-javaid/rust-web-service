093e44cdbded2d8d53955a8e8a3064533e0175db
ahsan-javaid <ahsan786cs@gmail.com>
2023-11-07 19:57:08 +0500

    wip: jwt auth verify

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use jsonwebtoken::{encode, Header, EncodingKey};
+use jsonwebtoken::{encode, Header, EncodingKey, decode, DecodingKey, Validation};
 use crate::models::user::*;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,4 +15,20 @@ pub fn create_jwt(user: &User) -> Result<String, jsonwebtoken::errors::Error> {
   };
 
    encode(&Header::default(), &my_claims, &EncodingKey::from_secret("secret".as_ref()))
+}
+
+fn verify_jwt_token(token: &str, decoding_key: &str) {
+  match decode::<serde_json::Value>(
+      &token,
+      &DecodingKey::from_secret(decoding_key.as_ref()),
+      &Validation::default(),
+  ) {
+      Ok(token_data) => {
+          println!("Token is valid: {:?}", token_data);
+          // Here, you can access token_data.claims to get the claims of the JWT
+      }
+      Err(e) => {
+          println!("Token is invalid: {:?}", e);
+      }
+  }
 }
